d853485b7c7475e6e9457370eaf1ed22f8d550e7
Severte <49499874+Severte@users.noreply.github.com>
2020-01-08 00:46:53 +0100

    Add files via upload

--- cnn.cpp
+++ cnn.cpp
@@ -2,17 +2,18 @@
 #include <fstream>
 #include <string>
 #include <ctime>
+#include <cmath>
 using namespace std;
 
 #define IMG_WIDTH 60
 #define IMG_HEIGHT 60
-#define FILTER_SIZE 9
-#define FILTER_SIZE_SQ 81
-#define FEATURE_MAP_SIZE 52
-#define FEATURE_MAP_SIZE_SQ 2704
-#define NEW_SIZE 26
-#define NEW_SIZE_SQ 676
-#define FILTER_COUNT 12
+#define FILTER_SIZE 7
+#define FILTER_SIZE_SQ 49
+#define FEATURE_MAP_SIZE 54
+#define FEATURE_MAP_SIZE_SQ 2916
+#define NEW_SIZE 27
+#define NEW_SIZE_SQ 729
+#define FILTER_COUNT 48 // must be divisible by 3 to account for 3 RGB levels
 
 
 float maxValue(float** filtersArray, int x, int y){
@@ -23,12 +24,15 @@ float maxValue(float** filtersArray, int x, int y){
     return result;
 }
 
+float sigmoid(float x){
+    return (1 / ( 1 + exp(-x)));
+}
 
 int dotProduct(float* img, float* filter, int x, int y, int c){
     float result = 0.0;
     for(int i = 0; i < FILTER_SIZE; i++){
         for(int j = 0; j < FILTER_SIZE; j++){
-            result += img[(x+i) * IMG_WIDTH + (y+j) + c] * filter[i * FILTER_SIZE + (y+j)];
+            result += img[(x+i) * IMG_WIDTH + (y+j)*3 + c] * filter[i * FILTER_SIZE + (y+j)];
         }
     }
     return max(0.0, result);
@@ -43,13 +47,13 @@ void applyFilter(float* img, float* filter, int c, float* result){
 }
 
 
-void filtersPooling(float** filtersArray, float* result){
+/*void filtersPooling(float** filtersArray, float* result){
     for(int i = 0; i <= IMG_WIDTH - FILTER_SIZE; i++){
         for(int j = 0; j <= IMG_HEIGHT - FILTER_SIZE; j++){
             result[i * FILTER_SIZE + j] = maxValue(filtersArray, i, j);
         }
     }
-}
+}*/
 
 void pooling(float* initial, float* result){
     int new_size = (IMG_WIDTH - FILTER_SIZE + 1 ) / 2
@@ -61,7 +65,7 @@ void pooling(float* initial, float* result){
     }
 }
 
-void hiddenLayer(float* input, float* weights, float* output){
+void hiddenLayer(float** input, float** weights, float* output){
     int result = 0.0;
     int new_size = (IMG_WIDTH - FILTER_SIZE + 1 ) / 2
     int new_size_sq = new_size * new_size;
@@ -69,7 +73,18 @@ void hiddenLayer(float* input, float* weights, float* output){
         for(int j = 0; i < new_size; j++){
             for(int k = 0; l < new_size; k++){
                 for(int l = 0; l < new_size; l++){
-                    output[i * new_size + j] = input[k * new_size + l] * weights[(i * new_size + j) * new_size_sq + (k * new_size + l)];
+                    for(int m = 0; m < FILTER_COUNT/3; m++){
+                        output[i * new_size + j] += input[m][k * new_size + l] * weights[m][(i * new_size + j) * new_size_sq + (k * new_size + l)];
+                    }
+                }
+            }
+        }
+    }
+    for(int i = 0; i < new_size; i++){
+        for(int j = 0; i < new_size; j++){
+            for(int k = 0; l < new_size; k++){
+                for(int l = 0; l < new_size; l++){
+                    output[i * new_size + j] = sigmoid(output[i * new_size + j]);
                 }
             }
         }
@@ -84,7 +99,7 @@ float outputLayer(float* input, float* weights){
             result += input[i * new_size + j] * weights [i * new_size + j];
         }
     }
-    return result;
+    return sigmoid(result);
 }
 
 void loadFilters(float** filters){
@@ -148,27 +163,38 @@ void saveFilters(float** filters){
         }
 }
 
-void initializeValues(float** filters, float* weights_hidden, float* weights_output){
+void initializeValues(float** filters, float** weights_hidden, float* weights_output){
     for(int i = 0; i < FILTER_COUNT; i++){
             for (int j = 0; j < FILTER_SIZE_SQ; j++){
                 filters[i][j] = random(-1.0,1.0);
             }
         }
-        for (int k = 0; k < NEW_SIZE_SQ; k++){
-                weights_hidden_layer[k] = random(-1.0,1.0);
-                weights_output_layer[k] = random(-1.0,1.0);
+    for (int k = 0; k < NEW_SIZE_SQ; k++){
+        for (int l = 0; l < FILTER_COUNT/3; l++){
+            weights_hidden_layer[l][k] = random(-1.0,1.0);
             }
+         weights_output_layer[k] = random(-1.0,1.0);
+        }
 }
 
 float performCNN(float* img, float** filters, float* weights_h, float* weights_o){
     float feature_maps[FILTER_COUNT][FEATURE_MAP_SIZE_SQ];
-    for(i = 0; i < FILTER_COUNT; i++){
+    for(int i = 0; i < FILTER_COUNT; i++){
         applyFilter(img, filters[i], i%3, feature_maps[i]);
     }
-    float filter_pooled[FEATURE_MAP_SIZE_SQ];
-    filtersPooling(feature_maps, filter_pooled);
-    float final_pooled[NEW_SIZE_SQ];
-    pooling(filter_pooled,final_pooled);
+    float feature_maps_sum[FILTER_COUNT/3][FEATURE_MAP_SIZE_SQ];
+    for(int i = 0; i < FILTER_COUNT/3; i++){
+        for(j = 0; j < FEATURE_MAP_SIZE_SQ; j++){
+            feature_maps_sum[i][j] = feature_maps[3*i][FEATURE_MAP_SIZE_SQ] + feature_maps[3*i+1][FEATURE_MAP_SIZE_SQ] + feature_maps[3*i+2][FEATURE_MAP_SIZE_SQ];
+        }
+        
+    }
+    /*float filter_pooled[FEATURE_MAP_SIZE_SQ];
+    filtersPooling(feature_maps, filter_pooled);*/
+    float final_pooled[FILTER_COUNT/3][NEW_SIZE_SQ];
+    for(int i = 0; i < FILTER_COUNT/3; i++){
+        pooling(feature_maps_sum[i],final_pooled[i]);
+    }
     float hidden_layer_output[NEW_SIZE_SQ];
     hiddenLayer(final_pooled, weights_h, hidden_layer_output);
     return outputLayer(hidden_layer_output, weights_o);
@@ -176,7 +202,7 @@ float performCNN(float* img, float** filters, float* weights_h, float* weights_o
 
 void main(){
     float filters[FILTER_COUNT][FILTER_SIZE_SQ];
-    float weights_hidden_layer[NEW_SIZE_SQ];
+    float weights_hidden_layer[FILTER_COUNT/3][NEW_SIZE_SQ];
     float weights_output_layer[NEW_SIZE_SQ];
     if(!fileExists("filter1.txt")){
         initializeValues(filters, weights_hidden_layer, weights_output_layer);
@@ -186,5 +212,5 @@ void main(){
         loadWeights(weights_hidden_layer, 1);
         loadWeights(weights_output_layer, 2);
     }
-    
+
 }
